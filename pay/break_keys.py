b5dfc0720a072061754a7467c2cfb9751ffd90d1
agm <amelia.meyer@leoni.com>
2022-07-21 09:26:23 -0400

    Helper files to break up the timesheet keys into dataclasses

--- pay/break_keys.py
+++ pay/break_keys.py
@@ -2,6 +2,7 @@ import inflection
 import json
 import pandas as pd
 
+
 def strip_type(key):
     if key[0] == key[0].lower():
         t = key[0]
@@ -10,22 +11,43 @@ def strip_type(key):
         t = ''
     return (t, key)
 
-if __name__ == '__main__':
-    d = json.load(open(r'D:\development\pyNOVATime\pay\2022-06-06--2022-06-19.json'))
-    ts = d['DataList'][0]
-    df = pd.DataFrame()
-    df.insert(0,'key',None,False)
-    df.insert(1,'type',None,True)
 
-    for key in ts:
+def make_df(tags):
+    df = pd.DataFrame()
+    df.insert(0, 'PYTHON_KEY', None, False)
+    df.insert(1, 'PYTHON_TYPE', None, True)
+    df.insert(2, 'PYTHON_CLASS', None, True)
+
+    for key in tags:
         t, k = strip_type(key)
         parts = inflection.underscore(k).split('_')
         for part in parts:
             if part not in df.columns:
-                df.insert(len(df.columns),part,False,True)
-        row = dict.fromkeys(parts,True)
-        row['type'] = t
-        row['key'] = key
+                df.insert(len(df.columns), part, pd.NA, True)
+        row = dict.fromkeys(parts, True)
+        row['PYTHON_TYPE'] = t
+        row['PYTHON_KEY'] = key
         df = df.append(pd.DataFrame(data=[row]))
-    print(df)
+    return df
+
+
+def reorder_columns(df):
+    new_columns = []
+    for column in df.columns:
+        if 'PYTHON' not in column:
+            try:
+                new_columns.append((column, df[column].value_counts()[True]))
+            except KeyError:
+                new_columns.append((column, 0))
+    new_columns = [tag[0]
+                   for tag in sorted(sorted(new_columns), reverse=True, key=lambda tup: tup[1])]
+    return df.reindex(columns=['PYTHON_KEY', 'PYTHON_TYPE', 'PYTHON_CLASS']+new_columns)
+
+
+if __name__ == '__main__':
+    d = json.load(
+        open(r'D:\development\pyNOVATime\pay\2022-06-06--2022-06-19.json'))
+    ts = d['DataList'][0]
+    df = make_df(ts)
+    df = reorder_columns(df)
     df.to_csv(r'D:\development\pyNOVATime\pay\keys.csv')
